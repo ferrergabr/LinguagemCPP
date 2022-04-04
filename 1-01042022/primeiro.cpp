8c2711d1e5dacb00e2435f8a61b31049d017908c
ferrergabr <ferrergabr78@gmail.com>
2022-04-04 08:55:32 -0300

    Estudo c++

---
+++ 1-01042022/primeiro.cpp
@@ -0,0 +1,15 @@
+/*
+Programa para exibir uma mensagem. O código foi escrito em c++
+*/
+/*
+Importação da biblioteca de entrada e saída de dados
+*/ 
+#include <iostream> 
+
+int main() {
+    
+    //saída padrão de dados 
+    std::cout<< "bem vindo ao c++\n";
+
+    return 0; 
+}
